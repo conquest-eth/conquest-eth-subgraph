be7cc82d7ec75aec34e6f839a51134d8287bf8c7
Wighawag <wighawag@gmail.com>
2020-11-07 18:25:23 +0000

    planetsCache and zones

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,9 @@
 import {
   store,
+  Bytes,
+  ByteArray,
   // Address,
-  // Bytes,
-  // BigInt,
+  BigInt,
   // BigDecimal,
 } from '@graphprotocol/graph-ts';
 import {
@@ -19,12 +20,73 @@ import {
   Fleet,
   ReinforcementArrived,
 } from '../generated/schema';
-// import {log} from '@graphprotocol/graph-ts';
+import {log} from '@graphprotocol/graph-ts';
 
 // const zeroAddress = '0x0000000000000000000000000000000000000000';
 
+function flipHex(str: string): string {
+  let newStr = '0x';
+  for (let i = 2; i < str.length; i++) {
+    let char = str.charAt(i);
+    if (char == '0') {
+      char = 'f';
+    } else if (char == '1') {
+      char = 'e';
+    } else if (char == '2') {
+      char = 'd';
+    } else if (char == '3') {
+      char = 'c';
+    } else if (char == '4') {
+      char = 'b';
+    } else if (char == '5') {
+      char = 'a';
+    } else if (char == '6') {
+      char = '9';
+    } else if (char == '7') {
+      char = '8';
+    } else if (char == '8') {
+      char = '7';
+    } else if (char == '9') {
+      char = '6';
+    } else if (char == 'a' || char == 'A') {
+      char = '5';
+    } else if (char == 'b' || char == 'B') {
+      char = '4';
+    } else if (char == 'c' || char == 'C') {
+      char = '3';
+    } else if (char == 'd' || char == 'D') {
+      char = '2';
+    } else if (char == 'e' || char == 'E') {
+      char = '1';
+    } else if (char == 'f' || char == 'F') {
+      char = '0';
+    }
+    newStr += char;
+  }
+  return newStr;
+}
+
+function c2(str: string): BigInt {
+  return BigInt.fromSignedBytes(
+    ByteArray.fromHexString(str).reverse() as Bytes
+  );
+
+  // if (str.charCodeAt(2) > 55) {
+  // > 7 (0111)
+  // str = flipHex(str);
+  // return BigInt.fromUnsignedBytes(ByteArray.fromHexString(str) as Bytes);
+  // .plus(
+  //   BigInt.fromI32(1)
+  // )
+  // .neg();
+  // } else {
+  //   return BigInt.fromUnsignedBytes(ByteArray.fromHexString(str) as Bytes);
+  // }
+}
+
 export function handlePlanetStake(event: PlanetStake): void {
-  const id = event.params.location.toHex();
+  const id = '0x' + event.params.location.toHex().slice(2).padStart(64, '0');
+  log.error('id: {}', [id]);
   let entity = AcquiredPlanet.load(id);
   if (!entity) {
     entity = new AcquiredPlanet(id);
@@ -36,6 +98,70 @@ export function handlePlanetStake(event: PlanetStake): void {
   entity.lastUpdated = event.block.timestamp;
   entity.productionRate = event.params.productionRate;
   entity.stake = event.params.newStake;
+  const yString = id.slice(0, 34);
+  const xString = '0x' + id.slice(34);
+
+  const x = c2(xString);
+  const absX = x.abs();
+  const signX = x.lt(BigInt.fromI32(-32))
+    ? BigInt.fromI32(-1)
+    : BigInt.fromI32(1);
+
+  log.error('(x,y): ({},{})', [xString, yString]);
+
+  const centerZoneX = absX.plus(BigInt.fromI32(32)).div(BigInt.fromI32(64));
+
+  const centerZoneXString = signX.equals(BigInt.fromI32(1))
+    ? centerZoneX.toHex().slice(2).padStart(32, '0')
+    : flipHex(
+        '0x' +
+          centerZoneX
+            .minus(BigInt.fromI32(1))
+            .toHexString()
+            .slice(2)
+            .padStart(32, '0')
+      ).slice(2);
+
+  const y = c2(yString);
+  const absY = y.abs();
+  const signY = y.lt(BigInt.fromI32(-32))
+    ? BigInt.fromI32(-1)
+    : BigInt.fromI32(1);
+
+  const centerZoneY = absY.plus(BigInt.fromI32(32)).div(BigInt.fromI32(64));
+
+  const centerZoneYString = signY.equals(BigInt.fromI32(1))
+    ? centerZoneY.toHex().slice(2).padStart(32, '0')
+    : flipHex(
+        '0x' +
+          centerZoneY
+            .minus(BigInt.fromI32(1))
+            .toHex()
+            .slice(2)
+            .padStart(32, '0')
+      ).slice(2);
+
+  entity.zone = '0x' + centerZoneYString + centerZoneXString;
+
+  entity.x = x;
+  entity.y = y;
+  entity.zoneX = signX.equals(BigInt.fromI32(1))
+    ? centerZoneX
+    : centerZoneX.neg();
+  entity.zoneY = signY.equals(BigInt.fromI32(1))
+    ? centerZoneY
+    : centerZoneY.neg();
+
+  log.error('zone: {}', [entity.zone]);
+
+  // entity.zone =
+  //   BigInt.fromI32(-2).toHex() +
+  //   '|||' +
+  //   BigInt.fromI32(-1).toHex() +
+  //   '|||' +
+  //   '0x' +
+  //   centerZoneX.toHexString().slice(2).padStart(32, '0') +
+  //   centerZoneY.toHexString().slice(2).padStart(32, '0');
   entity.save();
 }
 
@@ -90,7 +216,7 @@ export function handleAttack(event: Attack): void {
   entity.timestamp = event.block.timestamp;
   entity.save();
 
-  let planetEntity = AcquiredPlanet.load(id);
+  let planetEntity = AcquiredPlanet.load(id); // TODO
   if (!planetEntity) {
     planetEntity = new AcquiredPlanet(id);
     planetEntity.owner = fleetEntity.owner;
